7c57a66e2d783594f1d9e717a3973066bfd01641
Simone Carriero <simonecarriero@gmail.com>
2024-11-27 19:55:49 +0100

    Backtrack on topological sort of the DAG

--- src/autograd.rs
+++ src/autograd.rs
@@ -1,9 +1,11 @@
 use std::cell::RefCell;
+use std::collections::HashSet;
 use std::rc::Rc;
-
+use uuid::Uuid;
 
 #[derive(Debug, PartialEq)]
 struct Value {
+    label: Uuid,
     data: RefCell<f64>,
     grad: RefCell<f64>,
     back: Op,
@@ -17,15 +19,16 @@ enum Op {
 }
 
 fn value(data: f64) -> Rc<Value> {
-    Rc::new(Value { data: RefCell::new(data), grad: RefCell::new(0.0), back: Op::None })
+    value_with_grad(data, 0.0)
 }
 
 fn value_with_grad(data: f64, grad: f64) -> Rc<Value> {
-    Rc::new(Value { data: RefCell::new(data), grad: RefCell::new(grad), back: Op::None })
+    Rc::new(Value { label: Uuid::new_v4(), data: RefCell::new(data), grad: RefCell::new(grad), back: Op::None })
 }
 
 fn add(x: &Rc<Value>, y: &Rc<Value>) -> Rc<Value> {
     Rc::new(Value {
+        label: Uuid::new_v4(),
         data: RefCell::new(*x.data.borrow() + *y.data.borrow()),
         grad: RefCell::new(0.0),
         back: Op::Sum { x: Rc::clone(x), y: Rc::clone(y) },
@@ -34,13 +37,42 @@ fn add(x: &Rc<Value>, y: &Rc<Value>) -> Rc<Value> {
 
 fn mul(x: &Rc<Value>, y: &Rc<Value>) -> Rc<Value> {
     Rc::new(Value {
+        label: Uuid::new_v4(),
         data: RefCell::new(*x.data.borrow() * *y.data.borrow()),
         grad: RefCell::new(0.0),
         back: Op::Mul { x: Rc::clone(x), y: Rc::clone(y) },
     })
 }
 
+fn prev(value: &Rc<Value>) -> Vec<&Rc<Value>> {
+   match &value.back {
+       Op::None => vec![],
+       Op::Sum { x, y } => vec![x, y],
+       Op::Mul { x, y } => vec![x, y],
+   }
+}
+
+fn topological_sort<'a>(value: &'a Rc<Value>, topo: &mut Vec<&'a Rc<Value>>, visited: &mut HashSet<Uuid>) {
+    if !visited.contains(&value.label) {
+        visited.insert(value.label);
+        for v in prev(value) {
+            topological_sort(v, topo, visited)
+        }
+        topo.push(value)
+    }
+}
+
 fn backward(value: &Rc<Value>) {
+    *value.grad.borrow_mut() = 1.0;
+    let mut topo = vec![];
+    topological_sort(value, &mut topo, &mut HashSet::new());
+
+    for v in topo.iter().rev() {
+        backward_step(v);
+    }
+}
+
+fn backward_step(value: &Rc<Value>) {
     match &value.back {
         Op::None => {}
         Op::Sum { x, y } => {
@@ -48,16 +80,12 @@ fn backward(value: &Rc<Value>) {
             let y_local_derivative = 1.0;
             *x.grad.borrow_mut() += x_local_derivative * *value.grad.borrow();
             *y.grad.borrow_mut() += y_local_derivative * *value.grad.borrow();
-            backward(x);
-            backward(y);
         }
         Op::Mul { x, y } => {
             let x_local_derivative = *y.data.borrow();
             let y_local_derivative = *x.data.borrow();
             *x.grad.borrow_mut() += x_local_derivative * *value.grad.borrow();
             *y.grad.borrow_mut() += y_local_derivative * *value.grad.borrow();
-            backward(x);
-            backward(y);
         }
     }
 }
@@ -73,72 +101,61 @@ mod tests {
         let c = value(0.3);
         let x = add(&a, &mul(&b, &c));
 
-        assert_eq!(x, Rc::new(Value {
-            data: RefCell::new(0.16),
-            grad: RefCell::new(0.0),
-            back: Op::Sum {
-                x: value(0.1),
-                y: Rc::new(Value {
-                    data: RefCell::new(0.06),
-                    grad: RefCell::new(0.0),
-                    back: Op::Mul {
-                        x: value(0.2),
-                        y: value(0.3),
-                    },
-                }),
-            },
-        }));
+        assert_eq!(*x.data.borrow(), 0.16);
+        if let Op::Sum { x, y } = &x.back {
+            assert_eq!(*x.data.borrow(), 0.1);
+            assert_eq!(x.back, Op::None);
+            assert_eq!(*y.data.borrow(), 0.06);
+            if let Op::Mul { x, y} = &y.back {
+                assert_eq!(*x.data.borrow(), 0.2);
+                assert_eq!(*y.data.borrow(), 0.3);
+            } else {
+                assert!(false, "unexpected Op")
+            }
+        } else {
+            assert!(false, "unexpected Op")
+        }
     }
 
     #[test]
-    fn backward_on_sum_node_should_add_gradient_to_the_variables() {
+    fn backward_step_on_sum_node_should_add_gradient_to_the_variables() {
         let a = value_with_grad(0.1, 0.08);
         let b = value_with_grad(0.2, 0.58);
         let x = add(&a, &b);
         *x.grad.borrow_mut() = 0.42;
 
-        backward(&x);
+        backward_step(&x);
 
         assert_eq!(*a.grad.borrow(), 0.5);
         assert_eq!(*b.grad.borrow(), 1.0);
     }
 
     #[test]
-    fn backward_on_mul_node_should_add_gradient_to_the_variables() {
+    fn backward_step_on_mul_node_should_add_gradient_to_the_variables() {
         let a = value_with_grad(0.5, 0.05);
         let b = value_with_grad(0.25, 0.05);
         let x = mul(&a, &b);
         *x.grad.borrow_mut() = 0.8;
 
-        backward(&x);
+        backward_step(&x);
 
         assert_eq!(*a.grad.borrow(), 0.25);
         assert_eq!(*b.grad.borrow(), 0.45);
     }
 
     #[test]
-    fn backward_on_add_node_should_propagate_to_downstream_dag() {
-        let a = value(0.0);
-        let b = value(0.0);
-        let x = add(&add(&a, &b), &add(&a, &b));
-        *x.grad.borrow_mut() = 1.0;
+    fn backward_on_dag_should_execute_on_topological_sort() {
+        let x = value(3.0);
+        let y = value(3.0);
+        let z = value(2.0);
+        let a = mul(&x, &y);
+        let b = mul(&z, &a);
+        let c = add(&a, &b);
 
-        backward(&x);
+        backward(&c);
 
-        assert_eq!(*a.grad.borrow(), 2.0);
-        assert_eq!(*b.grad.borrow(), 2.0);
-    }
-
-    #[test]
-    fn backward_on_mul_node_should_propagate_to_downstream_dag() {
-        let a = value(0.2);
-        let b = value(0.3);
-        let x = mul(&add(&a, &b), &add(&a, &b));
-        *x.grad.borrow_mut() = 1.0;
-
-        backward(&x);
-
-        assert_eq!(*a.grad.borrow(), 1.0);
-        assert_eq!(*b.grad.borrow(), 1.0);
+        assert_eq!(*x.grad.borrow(), 9.0);
+        assert_eq!(*y.grad.borrow(), 9.0);
+        assert_eq!(*z.grad.borrow(), 9.0);
     }
 }
